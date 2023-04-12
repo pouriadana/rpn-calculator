835554e0be87dafbbb3a71ba69e2d18573138b18
pouria1337 <pouriadana73@gmail.com>
2023-04-12 20:55:17 +0330

    Add stack creation code along with some unfinished scaffolding for main logic

---
+++ main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> //c99
+
+/*Prototype*/
+bool push(int);
+int pop(void);
+bool is_empty(void);
+void stack_underflow();
+void stack_overflow();
+
+struct stack{
+    int n;
+    struct stack *next;
+};
+struct stack *top = NULL;
+
+int main()
+{
+    int i;
+    printf("--> ");
+    scanf(" %d", &i);
+    while(i != '=')
+    {
+        if(i == '*' || i == '+' ||)
+        
+        
+        scanf(" %d", &i);
+    }
+
+    while(i){
+        scanf(" %d", &i);
+
+        push(i);
+    }
+    while(top!= NULL)
+    {
+        printf("%d", top->n);
+        top = top->next;
+    }
+
+    return 0;
+}
+
+bool push(int n)
+{
+    struct stack *temp;
+    temp = malloc(sizeof(struct stack));
+    if (temp == NULL){
+        return false;
+    }
+    temp->n = n;
+    temp->next = top;
+    top = temp;
+    //free(temp);
+
+    return true;
+}
+
+int pop(void)
+{
+    int temp;
+    struct stack *s_temp;
+    if(is_empty())
+        stack_underflow();
+    s_temp = top;
+    temp = top->n;
+    top = top->next;
+    free(s_temp);
+    return temp;
+}
+
+bool is_empty()
+{
+    return top == NULL;
+}
+
+void stack_overflow(void)
+{
+    printf("Stack overflow\n");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void)
+{
+    printf("Stack underflow\n");
+    exit(EXIT_FAILURE);
+}
